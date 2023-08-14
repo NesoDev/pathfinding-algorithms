ce46cfa990f8a53c3fbf31d1263be203d2e6a5d2
Ever Frank <ever.avendano@unmsm.edu.pe>
2023-08-13 19:14:23 -0500

    first commit

---
+++ dijkstra_algorithm.py
@@ -0,0 +1,209 @@
+import heapq
+import random
+
+from pyray import *
+
+inf = float('inf')
+nodes = list()
+colors = [LIGHTGRAY, GRAY, DARKGRAY, YELLOW, GOLD, ORANGE, PINK, RED, MAROON, GREEN, LIME, DARKGREEN, SKYBLUE, BLUE, DARKBLUE, PURPLE, VIOLET, DARKPURPLE, BEIGE, BROWN, DARKBROWN,  WHITE,MAGENTA, RAYWHITE]
+
+class Node:
+    def __init__(self, iterator, x, y, radius, color):
+        self.id = iterator
+        self.position = [x, y]
+        self.radius = radius
+        self.color = color
+        self.nodes_adjacent = list()
+        self.state = True
+        self.labels = list()  # Lista de etiquetas [distance, node_ancestor, cycle]
+
+    def detectCollision(self, nodes):
+        for node in nodes:
+            if node != self and ((node.position[0] - self.position[0]) ** 2 + (node.position[1] - self.position[1]) ** 2) ** 0.5 <= 2 * self.radius:
+                return True
+        return False
+
+def createNodes(width, height, quantity, radius, color):
+    for iterator in range(quantity):
+        while True:
+            try:
+                x = random.randint(2 * radius, width - 2 * radius)
+                y = random.randint(radius, height - 2 * radius)
+                node = Node(iterator + 1, x, y, radius, color)
+                if not node.detectCollision(nodes):
+                    break
+            except:
+                pass
+        nodes.append(node)
+
+def view_nodes():
+    print("\n")
+    for node in nodes:
+        print(f"ID: {node.id} \n- position: {node.position[0]}, {node.position[1]} \n- nodes adjacent: ")
+        adjacent_ids = [str(node_adjacent.id) for node_adjacent in node.nodes_adjacent]
+        print(", ".join(adjacent_ids) + "\n")
+
+        labels = [f"[{label[0]}, {label[1].id if label[1] else None}, {label[2]}]" for label in node.labels]
+        print(", ".join(labels) + "\n")
+
+def draw_nodes():
+    for node in nodes:
+        draw_circle(int(node.position[0]), int(node.position[1]), node.radius, node.color)
+        font_size = node.radius
+        
+        text = f"{node.id}"
+        text_width = measure_text(text, int(font_size))
+        
+        pos_x_text = node.position[0] - text_width * 0.5
+        pos_y_text = node.position[1] - (1/3) * (font_size)
+        #pos_y_text = node.position[1] + 1.2 * node.radius
+        
+        draw_text(text, int(pos_x_text), int(pos_y_text), int(font_size), BLACK)
+
+def def_links():
+    for node in nodes:
+        print(f"\nNode {node.id}:")
+        quantity_links = int(input("- Cantidad de nodos adyacentes => "))
+        ids = []
+
+        for iterator in range(quantity_links):
+            id_valid = False
+            while not id_valid:
+                id = int(input(f"- id de nodo adyacente {iterator + 1} => "))
+                if id >= 1 and id <= len(nodes) and id != node.id:
+                    id_valid = True
+                else:
+                    print(
+                        "ID de nodo no válido. Debe estar entre 1 y la cantidad total de nodos, y no debe ser igual al ID del nodo actual."
+                    )
+            ids.append(id)
+
+        for id in ids:
+            node_adjacent = nodes[id - 1]
+            node.nodes_adjacent.append(node_adjacent)
+
+def draw_links():
+    font_size = 25 
+    color_index = 0
+    for node in nodes:
+        for node_adjacent in node.nodes_adjacent:
+            color = colors[color_index % len(colors)]
+            draw_line(node.position[0], node.position[1], node_adjacent.position[0], node_adjacent.position[1], color)
+            
+            mid_x = (node.position[0] + node_adjacent.position[0]) / 2
+            mid_y = (node.position[1] + node_adjacent.position[1]) / 2
+            
+            distance = calculate_distance(node, node_adjacent)
+            distance_text = f"{distance:.2f}"
+            
+            text_width = measure_text(distance_text, font_size)
+            text_x = mid_x - text_width / 2
+            text_y = mid_y - font_size / 2
+            draw_text(distance_text, int(text_x), int(text_y), font_size, WHITE)
+            
+            color_index += 1
+
+
+def dijkstra(id_node_initial):
+    pq = [(0, id_node_initial)]
+    heapq.heapify(pq)
+
+    while pq:
+        distance, current_id = heapq.heappop(pq)
+        current_node = nodes[current_id - 1]
+
+        if not current_node.labels:
+            current_node.labels.append([distance, None, 1])
+
+        if distance > current_node.labels[0][0]:
+            continue
+
+        for node_adjacent in current_node.nodes_adjacent:
+            new_distance = distance + calculate_distance(current_node, node_adjacent)
+
+            if not node_adjacent.labels:
+                node_adjacent.labels.append([inf, None, None])
+
+            if new_distance <= node_adjacent.labels[0][0]:
+                if new_distance < node_adjacent.labels[0][0]:
+                    node_adjacent.labels = []
+                node_adjacent.labels.append([new_distance, current_node, 1])
+                heapq.heappush(pq, (new_distance, node_adjacent.id))
+
+
+def calculate_distance(node1, node2):
+    return ((node1.position[0] - node2.position[0]) ** 2 + (node1.position[1] - node2.position[1]) ** 2) ** 0.5
+
+def lesser_route(id_node_final, current_path=None, all_paths=None):
+    if current_path is None:
+        current_path = []
+    if all_paths is None:
+        all_paths = []
+
+    node = nodes[id_node_final - 1]
+    current_path.append(node)
+
+    if node.labels[0][1] is None:
+        all_paths.append(current_path.copy())
+    else:
+        for previous_node_label in node.labels:
+            previous_node = previous_node_label[1]
+            if previous_node:
+                lesser_route(previous_node.id, current_path, all_paths)
+
+    current_path.pop()
+
+    return all_paths
+
+def view_routes(id_node_final):
+    routes = lesser_route(id_node_final)
+    for route in routes:
+        route.reverse()
+
+    min_distance = float('inf')  # Inicializa la distancia mínima como infinito
+    for route in routes:
+        distance = sum(calculate_distance(route[i], route[i+1]) for i in range(len(route)-1))
+        if distance < min_distance:
+            min_distance = distance
+
+    print("Rutas con distancia mínima:")
+    for iterator, route in enumerate(routes):
+        distance = sum(calculate_distance(route[i], route[i+1]) for i in range(len(route)-1))
+        if distance == min_distance:
+            print(f"\nRuta {iterator + 1}:")
+            ids_nodes = [str(node.id) for node in route]
+            print(" -> ".join(ids_nodes))
+
+
+def main():
+    width_window = 1000
+    height_window = 800
+    node_radius = 25
+
+    print("Ingrese datos:")
+    quantity = int(input("Cantidad de nodos => "))
+
+    createNodes(width_window - node_radius, height_window, quantity, node_radius, WHITE)
+
+    def_links()
+
+    id_node_initial = int(input("ID del nodo inicial => "))
+    id_node_final = int(input("ID del nodo final => "))
+
+    dijkstra(id_node_initial)
+
+    view_nodes()
+    view_routes(id_node_final)
+    
+    init_window(width_window, height_window, "Algorithm Dijkstra") 
+
+    while not window_should_close():
+        begin_drawing()
+        draw_links()
+        draw_nodes()
+        clear_background(BLACK)
+        end_drawing()
+    
+    close_window()
+
+main()
